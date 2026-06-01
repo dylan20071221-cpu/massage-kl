6a2fa8988d160618b7a7cdb720f33f3e6fe62d08
Dylan <user@users-MacBook-Air.local>
2026-06-02 00:41:41 +0800

    ✨ 改名：懂懂的管理后台

--- js/data.js
+++ js/data.js
@@ -4,7 +4,7 @@
  */
 
 const SITE_CONFIG = {
-  siteName: "大马顶级会所，顶级水疗",
+  siteName: "懂懂的管理后台",
   tagline: "我们服务宗旨：诚信经营：服务至上：怀着期待而来，装着满意而归，一次体验，此生无憾…",
   whatsappNumber: "60111220155",
   telegram: "",
